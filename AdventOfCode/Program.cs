697f5431170ccc3fea7d80eb193b72a8e3ee467a
Pietro Botnar <botnar.p@gmail.com>
2018-12-04 00:27:23 +0000

    Create 2018 directory

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using AdventOfCode._2018;
 
 namespace AdventOfCode
