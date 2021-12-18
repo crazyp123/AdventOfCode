48aa201a9af0101344bd0409e872100cda6f2e12
Pietro Botnar <botnar.p@gmail.com>
2021-12-18 16:04:33 +0000

    add day 8 2021 part 1

---
+++ AoC/y2021/Day08.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AoC.Utils;
+
+namespace AoC.y2021;
+
+public class Day08 : Day
+{
+    private List<(List<string>, List<string>)> _list;
+
+    public Day08()
+    {
+        // testInput
+        var input = "";
+
+        input = Input;
+
+        _list = input.AsListOf<string>().Select(line =>
+        {
+            var split = line.Split("|");
+
+            return (split[0].AsListOf<string>(" ", StringSplitOptions.RemoveEmptyEntries),
+                split[1].AsListOf<string>(" ", StringSplitOptions.RemoveEmptyEntries));
+        }).ToList();
+    }
+
+    public override object Result1()
+    {
+        var y = new[] { 2, 4, 3, 7 };
+        return _list.Sum(tuple => tuple.Item2.Count(s => y.Contains(s.Length)));
+    }
+
+    public override object Result2()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ AoC/y2021/Day09.cs
@@ -0,0 +1,25 @@
+﻿using AoC.Utils;
+
+namespace AoC.y2021
+{
+    public class Day09 : Day
+    {
+        public Day09()
+        {
+            // testInput
+            var input = "";
+
+            input = Input;
+        }
+
+        public override object Result1()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override object Result2()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
