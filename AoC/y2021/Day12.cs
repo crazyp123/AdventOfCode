e19ee1f0ce427f32d3579b40301170c7436e7ec2
pbotnar <Pietro.Botnar@glencore.co.uk>
2021-12-20 18:29:37 +0000

    add day 12 2021

--- AoC/y2021/Day12.cs
+++ AoC/y2021/Day12.cs
@@ -1,41 +1,76 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Utils;
 using QuikGraph;
-using QuikGraph.Algorithms;
-using QuikGraph.Algorithms.Search;
 
 namespace AoC.y2021
 {
     public class Day12 : Day
     {
-        private AdjacencyGraph<string, SEdge<string>> _graph;
+        private UndirectedGraph<string, SEdge<string>> _graph;
+
 
         public Day12()
         {
             // test input
-            var input = "";
+            var input = "start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end";
 
             input = Input;
 
             var list = input.AsListOfPatterns<string, string>("s-s");
-            _graph = list.Select(tuple => new SEdge<string>(tuple.Item1, tuple.Item2)).ToAdjacencyGraph<string, SEdge<string>>();
+            _graph = list.Select(tuple => new SEdge<string>(tuple.Item1, tuple.Item2)).ToUndirectedGraph<string, SEdge<string>>();
         }
 
         public override object Result1()
         {
-            var alg = new DepthFirstSearchAlgorithm<string, SEdge<string>>(_graph);
-
-            alg.SetRootVertex("start");
-
-            _graph.TryGetOutEdges("start", out var edges);
-
-
-            return 1;
+            return PathsToEnd("start",  new List<List<string>>(), new List<string>()).Count;
         }
 
         public override object Result2()
         {
-            throw new System.NotImplementedException();
+            return PathsToEnd("start", new List<List<string>>(), new List<string>(), true).Count;
         }
+
+        List<List<string>> PathsToEnd(string from, List<List<string>> paths, List<string> path, bool checkCount = false)
+        {
+            var thisPath = path.ToList();
+            thisPath.Add(from);
+
+            var nodes = _graph.AdjacentVertices(from);
+
+            foreach (var node in nodes)
+            {
+                if (char.IsLower(node[0]) && thisPath.Contains(node))
+                {
+                    if (!checkCount)
+                    {
+                        continue;
+                    }
+
+                    var maxCountReached = thisPath.Where(c => char.IsLower(c[0])).GroupBy(s => s).Any(g => g.Count() == 2);
+
+                    if (node == "start" || node == "end" || maxCountReached)
+                    {
+                        continue;
+                    }
+                }
+
+                if (node == "end")
+                {
+                    var x = thisPath.ToList();
+                        x.Add(node);
+                    paths.Add(x);
+                    continue;
+                }
+
+                PathsToEnd(node, paths, thisPath, checkCount);
+
+            }
+
+            return paths;
+        }
+
+
     }
 }
