a9b678d34b4bb8480c4991cecc3b159e81bda35e
pbotnar <Pietro.Botnar@glencore.co.uk>
2022-12-06 10:38:25 +0000

    add day 6

--- AoC/y2022/Day06.cs
+++ AoC/y2022/Day06.cs
@@ -1,20 +1,31 @@
-﻿using AoC.Utils;
+﻿using System.Linq;
+using AoC.Utils;
 
 namespace AoC.y2022;
 
 public class Day06 : Day
 {
-    public Day06()
-    {
-    }
 
     public override object Result1()
     {
-        throw new System.NotImplementedException();
+        return FindMarker(4);
     }
 
     public override object Result2()
     {
-        throw new System.NotImplementedException();
+        return FindMarker(14);
+    }
+
+    private object FindMarker(int x)
+    {
+        for (int i = 0; i < Input.Length - x; i++)
+        {
+            if (Input[i..(i + x)].Distinct().Count() == x)
+            {
+                return i + x;
+            }
+        }
+
+        return null;
     }
 }
