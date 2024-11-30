eae932f371d75ee34f20585dc97f153f15bffd6a
crazyp123 <botnar.p@gmail.com>
2024-11-30 23:50:17 +0000

    add days placeholders

---
+++ AoC/y2024/Day04.cs
@@ -0,0 +1,17 @@
+using System;
+using AoC.Utils;
+
+namespace AoC.y2024;
+
+public class Day04 : Day
+{
+    public override object Result1()
+    {
+        return Input;
+    }
+
+    public override object Result2()
+    {
+        throw new NotImplementedException();
+    }
+}
