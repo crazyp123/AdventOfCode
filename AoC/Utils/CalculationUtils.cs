eab0cb9a86970b986b77b870f697627f4146a901
crazyp123 <botnar.p@gmail.com>
2024-12-09 22:29:24 +0000

    add day 9 - part 2 incorrect

--- AoC/Utils/CalculationUtils.cs
+++ AoC/Utils/CalculationUtils.cs
@@ -1,50 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace AoC.Utils
+namespace AoC.Utils;
+
+public static class Calculations
 {
-    public static class Calculations
+    public static int ManhattanDistance(int x1, int y1, int x2, int y2)
     {
-        public static int ManhattanDistance(int x1, int y1, int x2, int y2)
-        {
-            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
-        }
+        return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+    }
 
-        public static int ManhattanDistance(Point a, Point b)
-        {
-            return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
-        }
+    public static int ManhattanDistance(Point a, Point b)
+    {
+        return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+    }
 
-        public static int Clamp(int value, int min, int max)
-        {
-            return (value < min) ? min : (value > max) ? max : value;
-        }
+    public static int Clamp(int value, int min, int max)
+    {
+        return value < min ? min : value > max ? max : value;
+    }
 
-        public static IEnumerable<(T, int)> FindAdjacentItems<T>(this IEnumerable<T> list)
+    public static IEnumerable<(T, int)> FindAdjacentItems<T>(this IEnumerable<T> list)
+    {
+        var previous = default(T);
+        var count = 0;
+        foreach (var item in list)
         {
-            T previous = default(T);
-            int count = 0;
-            foreach (var item in list)
+            if (item.Equals(previous))
             {
-                if (item.Equals(previous))
-                {
-                    count++;
-                }
-                else
-                {
-                    if (count > 1)
-                    {
-                        yield return (previous, count);
-                    }
-                    count = 1;
-                }
-                previous = item;
+                count++;
+            }
+            else
+            {
+                if (count > 1) yield return (previous, count);
+                count = 1;
             }
 
-            if (count > 1)
-            {
-                yield return (previous, count);
-            }
+            previous = item;
         }
+
+        if (count > 1) yield return (previous, count);
+    }
+
+    public static IEnumerable<(T value, int count, int index)> FindAdjacentItems<T>(this List<T> items)
+    {
+        var previous = default(T);
+        var count = 1;
+        for (var i = 0; i < items.Count; i++)
+        {
+            var item = items[i];
+            if (item.Equals(previous))
+            {
+                count++;
+            }
+            else if (i > 0)
+            {
+                yield return (previous, count, i - count);
+                count = 1;
+            }
+
+            previous = item;
+        }
+
+        yield return (previous, count, items.Count - count);
     }
 }
--- AoC/y2024/Day09.cs
+++ AoC/y2024/Day09.cs
@@ -1,17 +1,118 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Utils;
 
 namespace AoC.y2024;
 
 public class Day09 : Day
 {
+    private List<int?> _blocks;
+
+    public Day09()
+    {
+        var test = "2333133121414131402";
+        _blocks = GetBlocks(Input);
+    }
+
     public override object Result1()
     {
-        return Input;
+        var rearranged = ReArrange(_blocks);
+        return CheckSum(rearranged);
     }
 
     public override object Result2()
     {
-        throw new NotImplementedException();
+        var rearranged = ReArrangeFiles(_blocks);
+        return CheckSum(rearranged);
+    }
+
+    private List<int?> GetBlocks(string input)
+    {
+        var id = 0;
+        var blocks = new List<int?>();
+
+        var isBlock = true;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var c = input[i].AsInt();
+
+            var collection = Enumerable.Range(0, c).Select(_ => isBlock ? id as int? : null).ToList();
+            blocks.AddRange(collection);
+
+            if (isBlock) id++;
+
+            isBlock = !isBlock;
+        }
+
+        return blocks;
+    }
+
+    private int?[] ReArrange(List<int?> blocks)
+    {
+        var result = new int?[blocks.Count(i => i.HasValue)];
+
+        var reversed = blocks.Select((i, i1) => (num: i, ix: i1)).Where(i => i.num is not null).ToList();
+        reversed.Reverse();
+
+        for (var i = 0; i < blocks.Count; i++)
+        {
+            var current = blocks[i];
+            if (current is not null)
+            {
+                result[i] = current.Value;
+                if (i == result.Length - 1) break;
+                continue;
+            }
+
+            var (num, ix) = reversed.First();
+            result[i] = num.Value;
+            reversed.RemoveAt(0);
+        }
+
+        return result;
+    }
+
+    private int?[] ReArrangeFiles(List<int?> blocks)
+    {
+        var result = blocks.ToArray();
+
+        var blocksList = blocks.FindAdjacentItems().ToList();
+
+        var emptyBlocks = blocksList.Where(t => t.value is null).ToList();
+        var blocksToMove = blocksList.Where(t => t.value is not null).OrderByDescending(t => t.value.Value).ToList();
+
+        foreach (var (value, count, ix) in blocksToMove)
+        {
+            var empty = emptyBlocks.FirstOrDefault(t => t.count >= count);
+            if (empty == default) continue;
+
+            for (var i = 0; i < count; i++)
+            {
+                result[empty.index + i] = value;
+                result[ix + i] = null;
+            }
+
+            if (empty.count == count)
+                emptyBlocks.Remove(empty);
+            else
+                emptyBlocks[emptyBlocks.IndexOf(empty)] = (null, empty.count - count, empty.index + count);
+        }
+
+        return result.ToArray();
+    }
+
+    private ulong CheckSum(int?[] blocks)
+    {
+        ulong result = 0;
+        for (var i = 0; i < blocks.Length; i++)
+        {
+            var block = blocks[i];
+            if (block == null) continue;
+            result += (ulong)i * (ulong)block;
+        }
+
+        return result;
     }
 }
